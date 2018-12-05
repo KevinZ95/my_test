75ddf1868d72dc2d8db9bb210a724f074edd5f23
Jinwen <zhang1995jinwen@gmail.com>
2018-12-05 02:31:11 -0500

    2

--- spinlock.h
+++ spinlock.h
@@ -8,3 +8,18 @@ struct spinlock {
 	uint        pcs[10]; // The call stack (an array of program counters)
 	                     // that locked the lock.
 };
+
+
+// my mutex lock
+// basicly mutex_lock is spinlock
+// just added some fields to make it easy to use
+
+struct mutex_lock {
+    struct spinlock     myLock;
+    
+    uint    locked;         // 0 - unlocked, 1 - locked
+    char    name[100];      // lock name
+    int     owner;      // lock owner
+    int     lockID;         // lock id
+    int     status;        // 0 - unused, 1 - using
+};
